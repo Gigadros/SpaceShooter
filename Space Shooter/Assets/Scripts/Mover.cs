8d43b5ea581bf5e3ba709acdf286f32093b2f57f
Jordan Knight <jordan_r_knight@hotmail.com>
2017-03-23 19:13:10 +0900

    Lights, camera, background, player movement, shots
    Created lighting and set up the camera for the game. Added a background.
    Added a player controller script to the player to allow movement and
    shooting. Created a bolt prefab for the player's shots and added a
    boundary that can destroy shots as they leave the game view. Added a
    mover script and destroy by boundary script to handle moving and
    destroying shots.

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+	Rigidbody rb;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed, tilt, fireRate;
+	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+	Rigidbody rb;
+	float nextFire;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update (){
+		if(Input.GetButton("Fire1") && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3
+			(
+				Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+				0.0f,
+				Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+			);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
